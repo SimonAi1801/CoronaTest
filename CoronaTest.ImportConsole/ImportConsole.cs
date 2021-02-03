af64c0b9159a4ffa757c5cdfa031ff334d252f52
SimonAi1801 <56634821+SimonAi1801@users.noreply.github.com>
2021-02-03 16:57:28 +0100

    implemented importconsole

--- CoronaTest.ImportConsole/ImportConsole.cs
+++ CoronaTest.ImportConsole/ImportConsole.cs
@@ -1,10 +1,78 @@
-﻿using System;
+﻿using CoronaTest.Core.Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Utils;
 
 namespace CoronaTest.ImportConsole
 {
-    class ImportConsole
+    public class ImportController
     {
+        const string FileNameTestCenters = "TestCenter.csv";
+        const int Idx_TestCenterName = 0;
+        const int Idx_TestCenterCity = 1;
+        const int Idx_TestCenterPostalCode = 2;
+        const int Idx_TestCenterStreet = 3;
+        const int Idx_TestCenterSlotCapacity = 4;
+
+        const string FileNameCampaigns = "Kampagnen.csv";
+        const int Idx_CampaignName = 0;
+        const int Idx_From = 1;
+        const int Idx_To = 2;
+        const int Idx_TestCenters = 3;
+
+
+        public static IEnumerable<Campaign> ReadFromCsv()
+        {
+            string[][] matrix = MyFile.ReadStringMatrixFromCsv(FileNameTestCenters, true);
+
+            //Name; Stadt; Plz; Strasse; SlotKapazität
+            //Stadthalle Linz; Linz; 4020; Landstrasse 3; 7
+
+            var testCenters = matrix.Select(tc => new TestCenter
+            {
+                Name = tc[Idx_TestCenterName],
+                City = tc[Idx_TestCenterCity],
+                Postalcode = tc[Idx_TestCenterPostalCode],
+                Street = tc[Idx_TestCenterStreet],
+                SlotCapacity = int.Parse(tc[Idx_TestCenterSlotCapacity])
+
+            })
+            .ToDictionary(tc => tc.Name);
+
+            matrix = MyFile.ReadStringMatrixFromCsv(FileNameCampaigns, true);
+
+            //Kampagnenname; Von; Bis; TestCenter
+            //Antigentest Bildungsbereich OÖ; 01.10.2020; 14.10.2020; Stadthalle Linz, Stadthalle Leonding,Veranstaltungshalle Traun, Turnhalle Enns
+            //Antigentest Bildungsbereich Wien; 03.10.2020; 24.10.2020; Stadthalle Wien, Turnhalle Wien,Veranstaltungshalle Wien
+
+            var campaigns = matrix
+                .Select(c => new Campaign
+                {
+                    Name = c[Idx_CampaignName],
+                    From = DateTime.Parse(c[Idx_From]),
+                    To = DateTime.Parse(c[Idx_To]),
+                    AvailableTestCenters = c[Idx_TestCenters]
+                                    .Split(',')
+                                    .Select(tc => testCenters[tc])
+                                    .ToList()
+                })
+                .ToArray();
+
+            //foreach (var campaign in campaigns)
+            //{
+            //    foreach (var testCenter in campaign?.AvailableTestCenters)
+            //    {
+            //        if (testCenter.AvailableInCampaigns == null)
+            //        {
+            //            testCenter.AvailableInCampaigns = new List<Campaign>();
+            //        }
+            //        testCenter.AvailableInCampaigns.Add(campaign);
+            //    }
+            //}
+
+            return campaigns;
+        }
     }
 }
--- CoronaTest.ImportConsole/Program.cs
+++ CoronaTest.ImportConsole/Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using CoronaTest.Persistence;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CoronaTest.ImportConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await InitDataAsync();
+
+            Console.WriteLine();
+            Console.Write("Beenden mit Eingabetaste ...");
+            Console.ReadLine();
+        }
+
+        private static async Task InitDataAsync()
+        {
+            Console.WriteLine("***************************");
+            Console.WriteLine("          Import");
+            Console.WriteLine("***************************");
+            Console.WriteLine("Import der Teststationen in die Datenbank");
+            await using var unitOfWork = new UnitOfWork();
+            Console.WriteLine("Datenbank löschen");
+            await unitOfWork.DeleteDatabaseAsync();
+            Console.WriteLine("Datenbank migrieren");
+            await unitOfWork.MigrateDatabaseAsync();
+
+            Console.WriteLine("Daten werden von csv-Dateien eingelesen");
+            var campaigns = ImportController.ReadFromCsv().ToArray();
+            Console.WriteLine($"  {campaigns.Count()} Impfkampagnen eingelesen");
+
+            await unitOfWork.Campaigns.AddRangeAsync(campaigns);
+
+            Console.WriteLine("Impfkampagnen werden in Datenbank gespeichert");
+            await unitOfWork.SaveChangesAsync();
+
+            var cntCampaigns = await unitOfWork.Campaigns.GetCountAsync();
+            var cntTestCenters = await unitOfWork.TestCenters.GetCountAsync();
+
+            Console.WriteLine($"  Es wurden {cntCampaigns} Kampangnen gespeichert!");
+            Console.WriteLine($"  Es wurden {cntTestCenters} Testcenter gespeichert!");
         }
     }
 }
