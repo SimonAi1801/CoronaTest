a05dddb0cb49dd2e54b33d8a6d7632c18977bb2b
SimonAi1801 <56634821+SimonAi1801@users.noreply.github.com>
2021-02-03 16:25:10 +0100

    Add project files.

---
+++ CoronaTest.ImportConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoronaTest.ImportConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
