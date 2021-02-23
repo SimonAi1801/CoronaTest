b5840e1a31a5ec3e6f16754819e122d8dd4ef6ff
SimonAi1801 <56634821+SimonAi1801@users.noreply.github.com>
2021-02-23 21:22:15 +0100

    implemented dtos

--- CoronaTest.API/DTOs/StatisticsDto.cs
+++ CoronaTest.API/DTOs/StatisticsDto.cs
@@ -7,5 +7,11 @@ namespace CoronaTest.API.DTOs
 {
     public class StatisticsDto
     {
+        public string Year { get; set; }
+        public int CalenderWeek { get; set; }
+        public int CountOfExaminations { get; set; }
+        public int CountOfUnkownResults { get; set; }
+        public int CountofPositiveResults { get; set; }
+        public int CountOfNegativeResults { get; set; }
     }
 }
--- CoronaTest.API/DTOs/TestCenterDto.cs
+++ CoronaTest.API/DTOs/TestCenterDto.cs
@@ -7,5 +7,10 @@ namespace CoronaTest.API.DTOs
 {
     public class TestCenterDto
     {
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Postalcode { get; set; }
+        public string Name { get; set; }
+        public int SlotCapacity { get; set; }
     }
 }
